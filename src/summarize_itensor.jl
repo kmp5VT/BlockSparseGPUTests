07c5c301cf5fcc98c8ddeb3b56081c4a87611409
Karl Pierce <kmp5@vt.edu>
2024-04-17 12:14:41 -0400

    fix spelling error

--- src/summarize_itensor.jl
+++ src/summarize_itensor.jl
@@ -40,7 +40,7 @@ function summarize_index(i::Index{<:QNBlocks}; outputlevel::Int=0)
   )
   blockdims = [blockdim(i,j) for j in 1:nblocks(i)]
   println("(dim=$(dim(i))|id=$(id(i)%1000)|tags=$(tags(i))|dir=$(dir))")
-  if outpulevel >= 1 
+  if outputlevel >= 1 
     println("\t\t\tblockdims:$(blockdims)")
   end
 end
