5ad62452475c05e27f10c75f40b6937777219973
kmp5VT <kmp5@vt.edu>
2024-06-07 12:06:02 -0400

    Updating docs

--- docs/settings.jl
+++ docs/settings.jl
@@ -27,6 +27,7 @@ settings = Dict(
     #   "MPS Time Evolution" => "tutorials/MPSTimeEvolution.md",
     # ],
   ],
-  :format => Documenter.HTML(; prettyurls=false):doctest => true,
+  :format => Documenter.HTML(; assets=["assets/favicon.ico"], prettyurls=false),
+  :doctest => true,
   :checkdocs => :none,
 )
--- docs/src/tests.md
+++ docs/src/tests.md
@@ -1,6 +1,4 @@
 ```@docs
 test_1d_heisenberg
 test_2d_hubbard
-representative_contract_timing
-representative_svd_timing
 ```
